39290878d8cab05a80e3392e244c50dd7736045c
Aryak880 <57568703+Aryak880@users.noreply.github.com>
2021-12-05 16:15:40 +0530

    added commander npm

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose')
 
-mongoose.connect( process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI , {
     useUnifiedTopology: true,
     useCreateIndex: true,
     useNewUrlParser: true,
     useFindAndModify: false 
 })
+
+// "mongodb+srv://aryak1234:aryak1234@cluster0.jole8.mongodb.net/story?retryWrites=true&w=majority"
 
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 require('./db/mongoose.js')
 const cors = require('cors')
+require('dotenv').config()
 const express = require('express')
 const storyRoute = require('./routers/storyRoute.js')
 const userRoute = require('./routers/userRoute.js')
@@ -7,7 +8,6 @@ const app = express()
 const PORT = process.env.PORT || 3005
 
 app.use(cors())
-
 app.use(express.json())
 app.use(storyRoute)
 app.use(userRoute)
